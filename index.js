33bc4dc68500d7032156daea7feabbc1efa0beb9
Ahmad Awais <me@AhmadAwais.com>
2020-07-08 21:05:13 +0500

    📦 NEW: First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Cli Meow Help.
+ *
+ * Generate automatically formatted help text for &#x27;meow&#x27; CLI helper
+ *
+ * @author Awais <https://twitter.com/MrAhmadAwais/>
+ */
+module.exports = () => {};
---
+++ test.js
@@ -0,0 +1,3 @@
+const cliMeowHelp = require('./index.js');
+
+cliMeowHelp();
